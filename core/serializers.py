3a9aa2f7701146cbafbde871a38af40ad171fe12
YuZu <yurkazubkov@yandex.ru>
2022-12-17 20:46:18 +0300

    add login logic

---
+++ core/serializers.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from core.models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_repeat = serializers.CharField(write_only=True)
+
+    def validate(self, attrs: dict):
+        password = attrs.get("password")
+        password_repeat = attrs.pop("password_repeat", None)
+
+        if password != password_repeat:
+            raise ValidationError("password is not equal to password_repeat")
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        self.user = user
+        return user
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email", "password", "password_repeat",)
+
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs: dict):
+        username = attrs.get("username")
+        password = attrs.get("password")
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise ValidationError("username or password is incorrect")
+        attrs["user"] = user
+        return attrs
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email",)
--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import login
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.response import Response
 
 from core.models import User
-from core.serializers import CreateUserSerializer
+from core.serializers import CreateUserSerializer, LoginUserSerializer, UserSerializer
 
 
 class SignupView(CreateAPIView):
@@ -14,3 +15,18 @@ class SignupView(CreateAPIView):
     def perform_create(self, serializer):
         super().perform_create(serializer)
         login(self.request, user=serializer.user)
+
+
+class LoginView(GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        login(request, user=user)
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+
+
+
