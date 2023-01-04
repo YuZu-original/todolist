f72bdfe3d42914c4f9c6d9b87b9ea95d37ae9b75
YuZu <yurkazubkov@yandex.ru>
2023-01-04 17:39:41 +0300

    tg bot bug fix

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -17,9 +17,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         offset = 0
-        tg_client = TgClient("")
+
         while True:
-            res = tg_client.get_updates(offset=offset)
+            res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
                 self.handle_message(item.message)
